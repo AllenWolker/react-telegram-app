f0fdd8dad3bd445f7704fd2afe1af413dfe287b3
Valeriy <rikksandr@Gmail.com>
2024-08-19 22:48:28 +0300

    add post request

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WebApp from '@twa-dev/sdk'
+import axios from 'axios';
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [response, setResponse] = useState("");
+  const [data, setData] = useState({
+    "language": "original",
+    "period": "today"
+});
+  useEffect(() => {
+    axios
+      .post("https://poker247tech.ru/get_horoscope",data)
+      .then((response) => {
+        setResponse(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  console.log('data', response)
   return (
     <>
       <div>
